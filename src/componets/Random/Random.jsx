5df584418d62fd7909b7dd7071f56873075faced
Matute1975 <maserrano15@gmail.com>
2023-02-27 10:57:41 -0300

    Random con Scroll infinito

--- src/componets/Random/Random.jsx
+++ src/componets/Random/Random.jsx
@@ -2,6 +2,8 @@ import React, { useEffect,useState,useRef } from 'react'
 import axios from 'axios'
 import './Random.css'
 
+
+
 //const urlBasePhoto=`https://api.unsplash.com/photos/?page=${page}&per_page=30&client_id=tH1HMuBk-P-MztRQtyLP_kD7Rak7C6rSJcNAMCCq7wU` //maserrano
 //const urlBasePhoto=`https://api.unsplash.com/photos/random/?count=30&w=250&max-h=250fit=crop&orientation=landscape&w=250&max-h=250fit=crop&client_id=ghOVJdvH73_snz5IugwQ8FOLFsFmjceRTlo38dH6nqM` //matute1975
 //const urlBasePhoto=`https://api.unsplash.com/photos/random/?count=30&w=250&max-h=250fit=crop&orientation=landscape&w=250&max-h=250fit=crop&client_id=tH1HMuBk-P-MztRQtyLP_kD7Rak7C6rSJcNAMCCq7wU` //maserrano
@@ -10,6 +12,7 @@ const Random = () => {
     const [fotos,setFotos]=useState([])
     const [servidor,setServidor]=useState(-1000)
     const ultimaFotoRef=useRef(null)
+    const [siguiente,setSiguiente]=useState(0)
   
     useEffect(()=>{
     const obtenerFotos=async () =>{
@@ -21,7 +24,7 @@ const Random = () => {
       //.finally(()=>console.log('Finalizo la conexion a la API.'))
   }
   obtenerFotos()
-  },[])
+  },[siguiente])
   
   
   
@@ -31,17 +34,18 @@ const Random = () => {
     const fotosEnPantalla=document.querySelectorAll('#root .divRandom')
     ultimaFotoRef.current = fotosEnPantalla[fotos.length -1]
     //console.log('Esto tiene fondoDePantalla',fotosEnPantalla)
-    //console.log('Esto tiene current',ultimaFotoRef.current)
+    console.log('Esto tiene current',ultimaFotoRef.current)
     }
     if(ultimaFotoRef.current){
         let vigia = new IntersectionObserver((entries)=>{
             entries.forEach(entries=>{
                 if(entries.isIntersecting){
-                    
+                    setSiguiente(currentState=>(currentState+1))
+                    vigia.unobserve(ultimaFotoRef.current)
                 }
             })
         },{
-            rootMargin:'0px 0px 0px 0px',
+            rootMargin:'0px 0px 200px 0px',
             threshold:1.0
         })
         vigia.observe(ultimaFotoRef.current)
